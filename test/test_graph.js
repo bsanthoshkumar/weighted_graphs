2bbee3701b086718cc3153546c21699dd29a24cb
santhosh <santhosh.kumar@thoughtworks.com>
2020-06-21 15:24:49 +0530

    added tests for mst_prims

--- test/test_graph.js
+++ test/test_graph.js
@@ -27,4 +27,34 @@ describe('mst_prims', function () {
     };
     assert.deepStrictEqual(mst_prims(pairs, 'f'), expected);
   });
+
+  it('should give tree from vertex a', function () {
+    const expected = {
+      a: [['b', 5]],
+      b: [
+        ['d', 1],
+        ['e', 2],
+      ],
+      d: [['c', 1]],
+      c: [],
+      e: [['f', 2]],
+      f: [],
+    };
+    assert.deepStrictEqual(mst_prims(pairs, 'a'), expected);
+  });
+
+  it('should give tree from vertex c', function () {
+    const expected = {
+      c: [['d', 1]],
+      d: [['b', 1]],
+      b: [
+        ['e', 2],
+        ['a', 5],
+      ],
+      e: [['f', 2]],
+      f: [],
+      a: [],
+    };
+    assert.deepStrictEqual(mst_prims(pairs, 'c'), expected);
+  });
 });
